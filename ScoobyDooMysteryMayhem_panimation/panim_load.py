dd256862e3fd97e6e9aff9edbb0da0fc8fba0ee7
DashParrWare <107139901+DashParrWare@users.noreply.github.com>
2023-07-22 09:06:03 -0700

    Still Broken
    only mummy gets completed with this

--- ScoobyDooMysteryMayhem_panimation/panim_load.py
+++ ScoobyDooMysteryMayhem_panimation/panim_load.py
@@ -9,54 +9,38 @@ def panim_read(f, filepath):
     FrameRate = unpack(">f", f.read(4))[0] # multiply by 30 example 0.3333 is 1
     BoneCount = unpack("B", f.read(1))[0]
     bpy.context.scene.frame_end = int(FrameRate * 30)
-    if BoneCount == 35:
-        type1 = unpack("B", f.read(1))[0]
-        if type1 == 0:
-            type2 = unpack("B", f.read(1))[0]
-            if type2 == 0:
-                type3 = unpack("B", f.read(1))[0]
-                if type3 == 0:
-                    pass
-                if type3 == 1:
+    type1 = unpack("B", f.read(1))[0]
+    if type1 == 0:
+        type2 = unpack("B", f.read(1))[0]
+        if type2 == 0:
+            type3 = unpack("B", f.read(1))[0]
+            if type3 == 0:
+                for i in range(FileSize//58): # only mummy gets completed there needs to be a workaround to this???
+                    
                     Magic2 = unpack(">H", f.read(2))[0]
                     if Magic2 == 0:
-                        keyframeType = unpack(">H", f.read(2))[0]
-                        if keyframeType == 0:
-                            pass
-                        if keyframeType == 1:
-                            pass
-                        if keyframeType == 2:
-                            pass
-                        if keyframeType == 3:
-                            pass
-                        if keyframeType == 4:
-                            pass
-                        if keyframeType == 5:
-                            pass
-                        if keyframeType == 6:
-                            pass
-                        if keyframeType == 7:
-                            pass
-                        if keyframeType == 8:
-                            pass
-                        if keyframeType == 9:
-                            pass
-    if BoneCount == 38:
-        pass
-    if BoneCount == 43:
-        pass
-    if BoneCount == 45:
-        pass
-    if BoneCount == 46:
-        pass
-    if BoneCount == 47:
-        pass
-    if BoneCount == 55:
-        pass
-    if BoneCount == 56:
-        pass
-    if BoneCount == 57:
-        pass
+                        bone_id = unpack(">H", f.read(2))[0]
+                        KeyframeType = unpack(">H", f.read(2))[0]
+                        size = unpack(">H", f.read(2))[0]
+                    elif Magic2 == 4096:
+                        bone_id = unpack(">H", f.read(2))[0]
+                        KeyframeType = unpack(">H", f.read(2))[0]
+                        size = unpack(">H", f.read(2))[0]
+            if type3 == 1:
+                for i in range(FileSize//58): # only mummy gets completed there needs to be a workaround to this???
+                    
+                    Magic2 = unpack(">H", f.read(2))[0]
+                    if Magic2 == 0:
+                        bone_id = unpack(">H", f.read(2))[0]
+                        KeyframeType = unpack(">H", f.read(2))[0]
+                        size = unpack(">H", f.read(2))[0]
+                        print(f.tell())
+                    elif Magic2 == 4096:
+                        bone_id = unpack(">H", f.read(2))[0]
+                        KeyframeType = unpack(">H", f.read(2))[0]
+                        size = unpack(">H", f.read(2))[0]
+                        print(f.tell()) # nope not matching for some reason
+                        
 
 def panim_reading(filepath):
     with open(filepath, "rb") as f:
